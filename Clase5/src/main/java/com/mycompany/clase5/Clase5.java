fb89cf121fb0ad3df51b02cc945217bc90a5beda
Donald <rexgrady16@gmail.com>
2023-03-17 20:58:30 -0600

    Reasignación

---
+++ Clase5/src/main/java/com/mycompany/clase5/Clase5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clase5;
+
+/**
+ *
+ * @author rexgr
+ */
+public class Clase5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Clase5/src/main/java/com/mycompany/clase5/Nodo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.clase5;
+
+/**
+ *
+ * @author rexgr
+ */
+public class Nodo {
+    //atributos
+    int information;
+    Nodo anterior;
+    Nodo siguiente;
+    
+    //constructores
+    public Nodo (int dato){
+        informacion=dato;
+        anterior=null;
+        siguiente=null;
+    }
+}
